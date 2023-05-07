39729a513993092e0946d786b5d106642167958a
Daniel Goehring <danielgoehring73@gmail.com>
2023-05-06 21:12:09 -0400

    fixed media queries

--- src/Meal.js
+++ src/Meal.js
@@ -6,7 +6,7 @@ function Meal(props) {
             <div>
                 <p style={{fontSize: '32px', fontWeight: 'bold', marginBottom: '10px'}}>{props.mealName} {props.amount} oz</p>
             </div>
-            <div style={{fontSize: '24px', fontWeight: 'bold'}}>Calories: {props.calories} kcal</div>
+            <div style={{fontSize: '24px', fontWeight: 'bold', marginBottom: '40px'}}>Calories: {props.calories} kcal</div>
             <div >
                 <div className="foodinfo">
                     <div style={{
@@ -18,7 +18,10 @@ function Meal(props) {
                         display: 'inline-block',
                         padding: '80px 40px',
                         textAlign: 'center',
-                        marginBottom: '20px'
+                        marginBottom: '20px',
+                        marginLeft: '20px',
+                        marginRight: '20px',
+
                     }}>
                         <div style={{marginBottom: '20px'}}>Carbohydrates:</div>
                         <div >{props.carbs} g</div>
@@ -32,7 +35,9 @@ function Meal(props) {
                         display: 'inline-block',
                         padding: '80px 80px',
                         textAlign: 'center',
-                        marginBottom: '20px'
+                        marginBottom: '20px',
+                        marginLeft: '20px',
+                        marginRight: '20px',
                     }}>
                         <div  style={{marginBottom: '20px'}}>Protein:</div>
                         <div>{props.protein} g</div>
@@ -46,7 +51,9 @@ function Meal(props) {
                         display: 'inline-block',
                         padding: '80px 90px',
                         textAlign: 'center',
-                        marginBottom: '20px'
+                        marginBottom: '20px',
+                        marginLeft: '20px',
+                        marginRight: '20px',
                     }}>
                         <div  style={{marginBottom: '20px'}}>Fat:</div>
                         <div>{props.fat} g</div>
